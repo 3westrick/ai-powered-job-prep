eb2d2ce594a2981551de8691d79e11fd9daa8ebc
Arad Afraz <threewestrick@gmail.com>
2025-08-27 13:41:51 +0330

    pricing table

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next"
 import { Outfit } from "next/font/google"
 import "./globals.css"
-import ClerkProvider from "@/services/clerk/components/ClerkProvider"
+import ClerkProvider from "@/services/clerk/components/clerk-provider"
 import { ThemeProvider } from "next-themes"
 import { Toaster } from "@/components/ui/sonner"
 
