29127704b817ef0fe3303fc066ba5f29f7278462
Arad Afraz <threewestrick@gmail.com>
2025-09-04 13:04:32 +0330

    middleware 2

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,4 +1,4 @@
-import arcjet, { detectBot, shield, slidingWindow } from "@arcjet/next"
+// import arcjet, { detectBot, shield, slidingWindow } from "@arcjet/next"
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server"
 
 const isPublicRoute = createRouteMatcher([
@@ -7,28 +7,28 @@ const isPublicRoute = createRouteMatcher([
     "/api/webhooks(.*)",
 ])
 
-const aj = arcjet({
-    key: process.env.ARCJET_KEY!,
-    rules: [
-        shield({ mode: "LIVE" }),
-        detectBot({
-            mode: "LIVE", // Blocks requests. Use "DRY_RUN" to log only
-            // Block all bots except the following
-            allow: [
-                "CATEGORY:SEARCH_ENGINE", // Google, Bing, etc
-                // Uncomment to allow these other common bot categories
-                // See the full list at https://arcjet.com/bot-list
-                "CATEGORY:MONITOR", // Uptime monitoring services
-                "CATEGORY:PREVIEW", // Link previews e.g. Slack, Discord
-            ],
-        }),
-        slidingWindow({
-            mode: "LIVE",
-            interval: "1m",
-            max: 1000,
-        }),
-    ],
-})
+// const aj = arcjet({
+//     key: process.env.ARCJET_KEY!,
+//     rules: [
+//         shield({ mode: "LIVE" }),
+//         detectBot({
+//             mode: "LIVE", // Blocks requests. Use "DRY_RUN" to log only
+//             // Block all bots except the following
+//             allow: [
+//                 "CATEGORY:SEARCH_ENGINE", // Google, Bing, etc
+//                 // Uncomment to allow these other common bot categories
+//                 // See the full list at https://arcjet.com/bot-list
+//                 "CATEGORY:MONITOR", // Uptime monitoring services
+//                 "CATEGORY:PREVIEW", // Link previews e.g. Slack, Discord
+//             ],
+//         }),
+//         slidingWindow({
+//             mode: "LIVE",
+//             interval: "1m",
+//             max: 1000,
+//         }),
+//     ],
+// })
 
 export default clerkMiddleware(async (auth, req) => {
     // const decision = await aj.protect(req)
